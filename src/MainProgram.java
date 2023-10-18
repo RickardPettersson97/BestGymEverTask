4c84a21e669a274dfc2d5ae3f3cafec716e6f2ca
Rickard Pettersson <rickard971@hotmail.com>
2023-10-18 06:57:44 +0200

    Added reflections and bringing up some earlier issues, probably forgot a few

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -2,6 +2,30 @@ import java.util.List;
 
 public class MainProgram {
 
+    /**problem
+
+     getname, jag jämförde objekten och detta gav fel på testet, var jobbigt men kom på att till slut jämföra namnen istället
+     detta problemet var i eligable!
+
+     massor av logiska problem man behövde tänka på när man gör tester
+
+     Logiskt problem, hur ska man testa om användaren lägger in ett felaktigt namn eller felaktigt input?
+     Tänk om någon skrev exterminator300, ska detta vara "ogiltigt input" eller bara skriva "kunden har inte varit här tidigare?" väldigt svårt att
+     ta hand om det på rätt sätt, någon kanske heter exterminator300.
+     Bestämde mig för att allt som inte var del av personnumer eller namn på listan är "inte tidigare kund"
+
+     Gjorde en separatmetod för att testa att data som skickats med senddata faktiskt finns i listan som skickats till... hur testar jag denna metod?
+     Jag behöver ha data i en fil för att testa denna metod, men för att ha datat i filen behöver jag använda senddata som jag först vill testa med metoden som behöver senddata för att testas... om detta låter logiskt
+
+     lite svårt med vad som ska vara i vilken klass, fortfarande aningen nybörjare med upplägg, blir svårare med TDD!
+
+     Lite småproblem:
+
+     Fick nullpointerexception pga råkade skicka med null i senddata ifall kunden var tidigare kund men ICKE nuvarande kund, fixade genom system.exit(0) ifall tidigare kund
+
+     en till nullpointerexception vid första JOptionpane, om input var null, lade till ännu en systemexit ifall svaret var null.
+     */
+
     PersonInfo p;                                          //skapar upp instanser av klasser
     UserInput u = new UserInput();
     List<PersonInfo> theList;
@@ -25,10 +49,7 @@ public class MainProgram {
        fileio.sendData(p,false);                                 //här skickar det datat ifall man är medlem till en fil
 
 
-
    }
-
-
 
 
     public static void main(String[] args) {
